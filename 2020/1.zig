da5263f1a43d4cc1c21f9a21e2ad9a30e25688d2
Jan Halsema <janhlsm@gmail.com>
2020-12-02 01:54:17 +0100

    Minor day 1 correctness fixes

--- 2020/1.zig
+++ 2020/1.zig
@@ -4,20 +4,20 @@ const os = std.os;
 // Expected: Part 1: 719796 Part 2: 144554112
 
 pub fn run(input: Input) anyerror!Output {
-    // var timer = try Timer.start(); // DEBUG
-    // var time: u64 = 0; // DEBUG
+    var timer = try Timer.start(); // DEBUG
+    var time: u64 = 0; // DEBUG
 
     const target = 2020;
-    var set = [_]bool{false} ** 2020;
+    var set = [_]bool{false} ** (target + 1);
 
     var lines = mem.tokenize(input, "\n");
     while (lines.next()) |line| {
         set[try std.fmt.parseInt(u32, line, 10)] = true;
     }
     
-    // time = timer.lap(); // DEBUG
-    // print(">>> Parsing: {} ns\n", .{time}); // DEBUG
-    // time = timer.lap(); // DEBUG
+    time = timer.lap(); // DEBUG
+    print(">>> Parsing: {} ns\n", .{time}); // DEBUG
+    time = timer.lap(); // DEBUG
 
     var part1: i64 = undefined;
     for (set) | num, i | {
@@ -28,12 +28,12 @@ pub fn run(input: Input) anyerror!Output {
         }
     }
 
-    // time = timer.lap(); // DEBUG
-    // print(">>> Part 1: {} ns\n", .{time}); // DEBUG
-    // time = timer.lap(); // DEBUG
+    time = timer.lap(); // DEBUG
+    print(">>> Part 1: {} ns\n", .{time}); // DEBUG
+    time = timer.lap(); // DEBUG
 
     var part2: i64 = undefined;
-    outer: for (set) |num1, i| {
+    outer: for (set) | num1, i | {
         if (!num1) continue;
         for (set[i..target-i]) |num2, j| {
             const ij = i+j;
@@ -45,8 +45,8 @@ pub fn run(input: Input) anyerror!Output {
         }
     }
 
-    // time = timer.lap(); // DEBUG
-    // print(">>> Part 2: {} ns\n", .{time}); // DEBUG
+    time = timer.lap(); // DEBUG
+    print(">>> Part 2: {} ns\n", .{time}); // DEBUG
 
     return Output{.part1 = part1, .part2 = part2};
 }
