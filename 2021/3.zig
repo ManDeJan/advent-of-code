372f06cbacea8d9785997d1346c0be03e8533927
Jan Halsema <jan@mandejan.nl>
2021-12-03 22:14:57 +0100

    remove debug comments

--- 2021/3.zig
+++ 2021/3.zig
@@ -5,20 +5,8 @@ pub noinline fn run(input: aoc.Input) !aoc.Output {
     var part1: i64 = 0;
     var part2: i64 = 0;
 
-    // var timer = try aoc.Timer.start(); // DEBUG
-    // var time: u64 = 0; // DEBUG
-    // var init_time: u64 = 0; // DEBUG
-    // var part1_time: u64 = 0; // DEBUG
-    // var part2_time: u64 = 0; // DEBUG
-
-    // time = timer.lap(); // DEBUG
-
     var nums = try aoc.inputAsInts(u12, input, 2);
     defer nums.deinit();
-
-    // time = timer.lap(); // DEBUG
-    // init_time = time; // DEBUG
-    // time = timer.lap(); // DEBUG
 
     var bit_counts = [_]u32{0} ** 12;
     for (nums.items) |num| {
@@ -33,18 +21,9 @@ pub noinline fn run(input: aoc.Input) !aoc.Output {
     }
     part1 = @as(u32, gamma_rate) * ~gamma_rate;
 
-    // time = timer.lap(); // DEBUG
-    // part1_time = time; // DEBUG
-    // time = timer.lap(); // DEBUG
-
     const oxygen_rating = try filter_nums(nums.items, false);
     const co2_rating    = try filter_nums(nums.items, true);
-
     part2 = @as(u32, oxygen_rating) * co2_rating;
-
-    // time = timer.lap(); // DEBUG
-    // part2_time = time; // DEBUG
-    // aoc.print("Init:  {d:7} μs\nPart1: {d:7} μs\nPart2: {d:7} μs\n", .{init_time/1000, part1_time/1000, part2_time/1000}); // DEBUG
 
     return aoc.Output{.part1 = part1, .part2 = part2};
 }
