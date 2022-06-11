98a6aa25cb0e6a9159d07f8ce8b8e87a51304904
Jan Halsema <jan@mandejan.nl>
2022-06-11 13:12:37 +0200

    add day 3 & 4 2015

--- src/2015/3.zig
+++ src/2015/3.zig
@@ -1,25 +1,41 @@
 const std = @import("std");
 const aoc = @import("common.zig");
 
+const Point = packed struct {
+    x: i16,
+    y: i16,
+
+    pub fn move(self: *Point, dir: u8) void {
+             if (dir == '^') { self.y += 1; }
+        else if (dir == 'v') { self.y -= 1; }
+        else if (dir == '<') { self.x -= 1; }
+        else                 { self.x += 1; }
+    }
+};
+
 pub fn run(input: aoc.Input) !aoc.Output {
-    var part1: i64 = 0;
-    var part2: i64 = 0;
+    var part1: i64 = 1;
+    var part2: i64 = 1;
 
-    const Point = packed struct {x: i16, y: i16};
-    var map = std.AutoHashMap(u32, u32).init(aoc.allocator);
-    var cur_point = Point{.x = 0, .y = 0};
-    try map.put(@bitCast(u32, cur_point), 1);
+    var santa_map = std.AutoHashMap(u32, void).init(aoc.allocator);
+    var santa_pos = Point{.x = 0, .y = 0};
+    try santa_map.put(@bitCast(u32, santa_pos), {});
 
-    for (input) | dir | {
-             if (dir == '^') { cur_point.y += 1; }
-        else if (dir == 'v') { cur_point.y -= 1; }
-        else if (dir == '<') { cur_point.x -= 1; }
-        else                 { cur_point.x += 1; }
+    var santa_and_robot_map = std.AutoHashMap(u32, void).init(aoc.allocator);
+    var santa2_pos = Point{.x = 0, .y = 0};
+    var santaR_pos = Point{.x = 0, .y = 0};
+    try santa_and_robot_map.put(@bitCast(u32, santa2_pos), {});
 
-        var entry = try map.getOrPutValue(@bitCast(u32, cur_point), 1);
-        if (entry.value_ptr.* == 1) part1 += 1
-        else entry.value_ptr.* += 1;
+    for (input) | dir, i | {
+        santa_pos.move(dir);
+        var entry = try santa_map.getOrPut(@bitCast(u32, santa_pos));
+        if (!entry.found_existing) part1 += 1;
 
+        var santa_or_robot = &(if (i % 2 == 0) santa2_pos else santaR_pos);
+        santa_or_robot.move(dir);
+        var entry2 = try santa_and_robot_map.getOrPut(@bitCast(u32, santa_or_robot.*));
+        if (!entry2.found_existing) part2 += 1;
     }
     return aoc.Output{.part1 = part1, .part2 = part2};
 }
+
