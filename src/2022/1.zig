be2055c9312cc155ec582bfd6f7480433d77c5f1
Jan Halsema <jan@mandejan.nl>
2022-12-01 12:01:57 +0100

    Add part 2 day 1 2022

--- src/2022/1.zig
+++ src/2022/1.zig
@@ -6,10 +6,12 @@ pub fn run(input: aoc.Input) !aoc.Output {
     var part2: i64 = undefined;
 
     var elf_segments = aoc.split(input, "\n\n");
-    var max_calories: u32 = 0;
-    var elf_index: u32 = undefined;
 
-    {var i: u32 = 1; while (elf_segments.next()) |elf_segment| : (i += 1) {
+    var top1: u32 = 0;
+    var top2: u32 = 0;
+    var top3: u32 = 0;
+
+    while (elf_segments.next()) |elf_segment| {
         var calorie_lines = aoc.tokenize(elf_segment, "\n");
         var total_calories: u32 = 0;
 
@@ -17,21 +19,22 @@ pub fn run(input: aoc.Input) !aoc.Output {
             total_calories += try std.fmt.parseInt(u32, calorie_line, 10);
         }
 
-        // aoc.print("Elf: {d:3}, Cals: {d:5}\n", .{i, total_calories});
+        insertInTop3(&top1, &top2, &top3, total_calories);
+    }
 
-        if (max_calories < total_calories) {
-            max_calories = total_calories;
-            elf_index = i;
-        }
-    }}
-
-    part1 = max_calories;
-    part2 = 0;
+    part1 = top1;
+    part2 = top1 + top2 + top3;
     return aoc.Output{ .part1 = part1, .part2 = part2 };
 }
 
+inline fn insertInTop3(top1: *u32, top2: *u32, top3: *u32, new: u32) void {
+         if (new > top1.*) { top3.* = top2.*; top2.* = top1.*; top1.* = new; }
+    else if (new > top2.*) { top3.* = top2.*; top2.* = new; }
+    else if (new > top3.*) { top3.* = new; }
+}
+
 test "2022-1" {
-    try aoc.testPart1(4, run(
+    try aoc.testBoth(24000, 45000, run(
         \\1000
         \\2000
         \\3000
@@ -47,5 +50,4 @@ test "2022-1" {
         \\
         \\10000
     ));
-    try aoc.testPart2(0, run(""));
 }
