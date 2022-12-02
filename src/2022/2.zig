6806d2a2ea9ffe98420b88a24f0e40f674b9d84a
Jan Halsema <jan@mandejan.nl>
2022-12-02 15:58:13 +0100

    improve day 2 2022 performance

--- src/2022/2.zig
+++ src/2022/2.zig
@@ -7,35 +7,14 @@ pub fn run(input: aoc.Input) !aoc.Output {
 
     const lines = std.mem.bytesAsSlice([4]u8, input);
     for (lines) |line| {
-        switch (matchToInt(line[0], line[2])) {
-            //                               W/L Shp         W/L Shp
-            matchToInt('B', 'X') => {part1 += 0 + 1; part2 += 0 + 1;},
-            matchToInt('C', 'Y') => {part1 += 0 + 2; part2 += 3 + 3;},
-            matchToInt('A', 'Z') => {part1 += 0 + 3; part2 += 6 + 2;},
-            matchToInt('A', 'X') => {part1 += 3 + 1; part2 += 0 + 3;},
-            matchToInt('B', 'Y') => {part1 += 3 + 2; part2 += 3 + 2;},
-            matchToInt('C', 'Z') => {part1 += 3 + 3; part2 += 6 + 1;},
-            matchToInt('C', 'X') => {part1 += 6 + 1; part2 += 0 + 2;},
-            matchToInt('A', 'Y') => {part1 += 6 + 2; part2 += 3 + 1;},
-            matchToInt('B', 'Z') => {part1 += 6 + 3; part2 += 6 + 3;},
-            else => unreachable,
-        }
+        const a: u8 = line[0]; const b: u8 = line[2];
+        if (!(a == 'A' or a == 'B' or a == 'C') and
+            !(b == 'X' or b == 'Y' or b == 'Z')) unreachable;
+        part1 +=      b - 87  + 3 * ((b - a - 1) % 3);
+        part2 += 1 + (b - 88) * 3 +  (a + b - 1) % 3;
     }
 
     return aoc.Output{.part1 = part1, .part2 = part2};
-}
-
-fn matchToInt(a: u8, b: u8) u4 {
-    if (a == 'B' and b == 'X') return 1;
-    if (a == 'C' and b == 'Y') return 2;
-    if (a == 'A' and b == 'Z') return 3;
-    if (a == 'A' and b == 'X') return 4;
-    if (a == 'B' and b == 'Y') return 5;
-    if (a == 'C' and b == 'Z') return 6;
-    if (a == 'C' and b == 'X') return 7;
-    if (a == 'A' and b == 'Y') return 8;
-    if (a == 'B' and b == 'Z') return 9;
-    unreachable;
 }
 
 test "2022-2" {
