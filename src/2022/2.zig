e306639471941fb7506d5ff77b2a1a6569e8efe7
Jan Halsema <jan@mandejan.nl>
2022-12-05 17:25:31 +0100

    fix day 2 2022 assert

--- src/2022/2.zig
+++ src/2022/2.zig
@@ -7,15 +7,15 @@ pub fn run(input: aoc.Input) !aoc.Output {
 
     const lines = std.mem.bytesAsSlice([4]u8, input);
     for (lines) |line| {
-        const a: u8 = line[0]; const b: u8 = line[2];
+        const a: u8 = line[0];
+        const b: u8 = line[2];
         // Help out the optimizer by asserting our input is always constrained
-        std.debug.assert(!(a == 'A' or a == 'B' or a == 'C') and
-                         !(b == 'X' or b == 'Y' or b == 'Z'));
+        std.debug.assert(a >= 'A' and a <= 'C' and b >= 'X' and b <= 'Z');
         part1 += 1 + (b - 'X') + 3 * ((b - a - 1) % 3);
         part2 += 1 + (b - 'X') * 3 + ((a + b - 1) % 3);
     }
 
-    return aoc.Output{.part1 = part1, .part2 = part2};
+    return aoc.Output{ .part1 = part1, .part2 = part2 };
 }
 
 test "2022-2" {
