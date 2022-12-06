4585197210ebcaf0377748e733433bc8785732d0
Jan Halsema <jan@mandejan.nl>
2022-12-06 15:19:07 +0100

    add day 3 2022

--- src/2022/5.zig
+++ src/2022/5.zig
@@ -28,14 +28,14 @@ pub fn run(input: aoc.Input, part1: []u8, part2: []u8) !void {
     defer for (stacks1.items) |s| {s.deinit();};
     defer for (stacks2.items) |s| {s.deinit();};
 
-    {var i: usize = 0; while (header_sections.next()) |box_line| : (i += 1) {
+    {var i: usize = 0; while (header_sections.next()) |box_line| : (i += 1)
         {var j: usize = 0; while (j < stack_count) : (j += 1) {
             const letter = box_line[1 + 4 * j];
             if (letter == ' ') continue;
             stacks1.items[j].appendAssumeCapacity(letter);
             stacks2.items[j].appendAssumeCapacity(letter);
         }}
-    }}
+    }
 
     while (instructions.next()) |instruction| {
         var instruction_tokens = aoc.tokenize(instruction, "ft mover"); // All letters in words used in instruction lines
@@ -43,16 +43,16 @@ pub fn run(input: aoc.Input, part1: []u8, part2: []u8) !void {
         const src = try std.fmt.parseUnsigned(u8, instruction_tokens.next().?, 10);
         const dst = try std.fmt.parseUnsigned(u8, instruction_tokens.next().?, 10);
 
-        {var i: usize = 0; while (i < amt) : (i += 1) {
+        {var i: usize = 0; while (i < amt) : (i += 1)
             stacks1.items[dst-1].appendAssumeCapacity(stacks1.items[src-1].pop());
-        }}
+        }
         const len = stacks2.items[src-1].items.len;
         stacks2.items[dst-1].appendSliceAssumeCapacity(stacks2.items[src-1].items[len-amt..len]);
         stacks2.items[src-1].shrinkRetainingCapacity(len - amt);
     }
 
-    for (stacks1.items) |s, i| { part1[i] = s.items[s.items.len - 1]; }
-    for (stacks2.items) |s, i| { part2[i] = s.items[s.items.len - 1]; }
+    for (stacks1.items) |s, i| part1[i] = s.items[s.items.len - 1];
+    for (stacks2.items) |s, i| part2[i] = s.items[s.items.len - 1];
 }
 
 test "2022-5" {
