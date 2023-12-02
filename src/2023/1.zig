79fd08f80ba4f5eed86bb15ebc0dbc40c67dceaf
Jan Halsema <jan@mandejan.nl>
2023-12-02 23:39:28 +0100

    improve performance day 1 2023

--- src/2023/1.zig
+++ src/2023/1.zig
@@ -7,7 +7,7 @@ pub fn run(input: aoc.Input) !aoc.Output {
 
     var calibration_codes = aoc.splitLines(input);
 
-    const digit_strings = .{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    const digit_strings = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
     while (calibration_codes.next()) |code| {
         var first_digit_1: u32 = 0;
@@ -15,23 +15,38 @@ pub fn run(input: aoc.Input) !aoc.Output {
         var last_digit_1: u32 = 0;
         var last_digit_2: u32 = 0;
 
-        for (code, 0..) |_, i| {
+        for (0..code.len) |i| {
             if (code[i] >= '1' and code[i] <= '9') {
                 const digit = code[i] - '0';
-                if (first_digit_1 == 0) first_digit_1 = digit;
+                first_digit_1 = digit;
                 if (first_digit_2 == 0) first_digit_2 = digit;
-                last_digit_1 = digit;
-                last_digit_2 = digit;
-            } else {
+                break;
+            } else if (first_digit_2 == 0) {
                 inline for (digit_strings, 1..) |digit_string, digit| {
                     if (std.mem.startsWith(u8, code[i..], digit_string)) {
-                        if (first_digit_2 == 0) first_digit_2 = digit;
-                        last_digit_2 = digit;
+                        first_digit_2 = digit;
+                        break;
                     }
                 }
             }
         }
-
+        for (0..code.len) |i_| {
+            const i: usize = code.len - i_ - 1;
+            if (code[i] >= '1' and code[i] <= '9') {
+                const digit = code[i] - '0';
+                last_digit_1 = digit;
+                if (last_digit_2 == 0) last_digit_2 = digit;
+                break;
+            } else if (i_ >= 3 and last_digit_2 == 0) {
+                inline for (digit_strings, 1..) |digit_string, digit| {
+                    if (std.mem.startsWith(u8, code[i..], digit_string)) {
+                        last_digit_2 = digit;
+                        break;
+                    }
+                }
+            }
+        }
+        // aoc.print("{s}\n{}, {} - {}, {}\n\n", .{code, first_digit_1, last_digit_1, first_digit_2, last_digit_2});
         part1 += first_digit_1 * 10 + last_digit_1;
         part2 += first_digit_2 * 10 + last_digit_2;
     }
