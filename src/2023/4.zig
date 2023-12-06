faae914e9714a4679aea736aedc1911f518ec14e
Jan Halsema <jan@mandejan.nl>
2023-12-06 18:24:18 +0100

    simd alternatives day 4 2023

--- src/2023/4.zig
+++ src/2023/4.zig
@@ -60,6 +60,7 @@ fn scratchCards(
             }
             break :blk lucky_nums;
         };
+        _ = lucky_nums;
         const scratch_nums = blk: {
             var scratch_nums: [scratch_n]u16 = undefined;
             inline for (0..scratch_n) |n| {
@@ -67,19 +68,26 @@ fn scratchCards(
             }
             break :blk scratch_nums;
         };
+        _ = scratch_nums;
 
-        var matching: u32 = 0;
+        const matching: u32 = 0;
         // const scratch_vec: @Vector(scratch_n, u16) = scratch_nums;
         // for (lucky_nums) |l| {
         //     const lucky_vec: @Vector(scratch_n, u16) = @splat(l);
         //     matching += @intFromBool(@reduce(.Or, scratch_vec == lucky_vec));
         // }
 
-        for (lucky_nums) |l| {
-            inline for (scratch_nums) |s| {
-                if (l == s) matching += 1;
-            }
-        }
+        // const lucky_vec: @Vector(lucky_n, u16) = lucky_nums;
+        // for (lucky_nums) |l| {
+        //     const scratch_vec: @Vector(lucky_n, u16) = @splat(l);
+        //     matching += @intFromBool(@reduce(.Or, scratch_vec == lucky_vec));
+        // }
+
+        // for (lucky_nums) |l| {
+        //     inline for (scratch_nums) |s| {
+        //         if (l == s) matching += 1;
+        //     }
+        // }
 
         part1 += if (matching > 0) std.math.pow(i64, 2, matching - 1) else 0;
         for (i / length..matching + i / length) |j|
